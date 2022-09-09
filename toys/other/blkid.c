6163c36ef5f2424d5c712c71ce35b6fe8038ea45
xuxinyu <xuxinyu6@huawei.com>
2022-09-09 15:19:37 +0800

    toybox部分命令升级（blkid vmstat bootchartd crond vi wget xzcat）
    Signed-off-by: xuxinyu <xuxinyu6@huawei.com>
    Change-Id: I7c71559d536213423606a9be72b206826cb9bc9c

--- toys/other/blkid.c
+++ toys/other/blkid.c
@@ -73,7 +73,7 @@ static void show_tag(char *key, char *value)
     for (al = TT.s; al; al = al->next) if (!strcmp(key, al->arg)) show = 1;
   } else show = 1;
 
-  if (show) printf(" %s=\"%s\"", key, value);
+  if (show && *value) printf(" %s=\"%s\"", key, value);
 }
 
 static void flagshow(char *s, char *name)
@@ -197,12 +197,11 @@ void blkid_main(void)
   if (*toys.optargs && !FLAG(L) && !FLAG(U)) loopfiles(toys.optargs, do_blkid);
   else {
     unsigned int ma, mi, sz, fd;
-    char *name = toybuf, *buffer = toybuf+1024, device[32];
+    char name[32], device[5+32];
     FILE *fp = xfopen("/proc/partitions", "r");
 
-    while (fgets(buffer, 1024, fp)) {
-      *name = 0;
-      if (sscanf(buffer, " %u %u %u %[^\n ]", &ma, &mi, &sz, name) != 4)
+    while (fgets(toybuf, sizeof(toybuf), fp)) {
+      if (sscanf(toybuf, " %u %u %u %31s", &ma, &mi, &sz, name) != 4)
         continue;
 
       sprintf(device, "/dev/%.20s", name);
