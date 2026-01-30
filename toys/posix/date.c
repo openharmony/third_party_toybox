596eb972695c55f109a9e8770ab2c341ba3e6a79
gong-guilin <gongguilin@h-partners.com>
2026-01-30 15:12:20 +0800

    add
    Signed-off-by: gong-guilin <gongguilin@h-partners.com>

--- toys/posix/date.c
+++ toys/posix/date.c
@@ -60,11 +60,7 @@ config DATE
 #include "toys.h"
 
 GLOBALS(
-#ifdef TOYBOX_OH_ADAPT
-  char *s, *r, *I, *d;
-#else
   char *s, *r, *I, *D, *d;
-#endif
 
   unsigned nano;
 )
@@ -156,16 +152,12 @@ void date_main(void)
   }
 
   if (TT.d) {
-#ifdef TOYBOX_OH_ADAPT
-    parse_date(TT.d, &t);
-#else
     if (TT.D) {
       struct tm tm = {};
       char *s = strptime(TT.d, TT.D+(*TT.D=='+'), &tm);
 
       t = (s && !*s) ? xvali_date(&tm, s) : xvali_date(0, TT.d);
     } else parse_date(TT.d, &t);
-#endif
   } else {
     struct timespec ts;
     struct stat st;
