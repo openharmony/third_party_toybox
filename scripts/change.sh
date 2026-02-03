379e9e975dd73c85b1d2f00348a05ab0215db21f
gong-guilin <gongguilin@h-partners.com>
2026-02-03 17:26:09 +0800

    update script
    Signed-off-by: gong-guilin <gongguilin@h-partners.com>

--- scripts/change.sh
+++ scripts/change.sh
@@ -2,20 +2,18 @@
 
 # build each command as a standalone executable
 
+source scripts/portability.sh
+
 NOBUILD=1 scripts/make.sh > /dev/null &&
-${HOSTCC:-cc} -I . scripts/install.c -o generated/instlist &&
+${HOSTCC:-cc} -I . scripts/install.c -o "$UNSTRIPPED"/instlist &&
 export PREFIX=${PREFIX:-change/} &&
 mkdir -p "$PREFIX" || exit 1
 
-# Build all the commands standalone except:
-
-# sh - shell builtins like "cd" and "exit" need the multiplexer
-# help - needs to know what other commands are enabled (use command --help)
-
-for i in $(generated/instlist | egrep -vw "sh|help")
+# Build all the commands standalone
+for i in $("$UNSTRIPPED"/instlist)
 do
   echo -n " $i" &&
-  scripts/single.sh $i > /dev/null 2>$PREFIX/${i}.bad &&
+  scripts/single.sh $i &>$PREFIX/${i}.bad &&
     rm $PREFIX/${i}.bad || echo -n '*'
 done
 echo
